c62cbb7e430c7bf98255c66133eef8b826708ca9
Bruce Campbell <blackhat00@gmail.com>
2022-03-19 00:13:28 -0700

    Final commit for this assessment before turning in

--- src/test_script/qwick.test.js
+++ src/test_script/qwick.test.js
@@ -11,7 +11,6 @@ import 'jest';
 //const url = 'http://localhost:3000';
 const regUrl = 'http://localhost:3000/#/register';
 
-
 // Quick and dirty name generator for testing purposes while writing this.
 // In a more mature setup this would be refined a bit and could be used for performance,
 // load, and with more refinement international, character set, and boundary equivalance.
@@ -35,26 +34,74 @@ function mapName () {
 let userName = mapName();
 let userPw = 'secure';
 let userEmail = userName + '@testmail.com';
+let title = 'Test Post by ' + userName;
+let about = 'Testing Apps';
+let article = 'I test therefore you work';
 
 // Open page, start directly with register
 // QA Note: This would normally be built for all browwsers
 (async () => {
+    // Linear run setup via Selenium to test navigation and allow visibility to testing process
+    // Typically this would also use a series of screenshots for archived validation and research
+
+    //Initial setup and fetch page
     let browser = await new Builder().forBrowser('chrome').build();
     await browser.manage().setTimeouts( { implicit: 10000, pageLoad: 10000, script: 10000} );
     await browser.get(regUrl);
+
+    // Send values for new user then submit (Sign Up button)
+    // linear repeating commands is more for readability on a small app like this, for re-useability or
+    // larger test sets this would be an array of tuples run through a loop.
     await browser.findElement(By.xpath("/html/body/div[1]/div/div/div/div/form/fieldset/fieldset[1]/input")).sendKeys(userName);
     await browser.findElement(By.xpath("/html/body/div[1]/div/div/div/div/form/fieldset/fieldset[2]/input")).sendKeys(userEmail);
     await browser.findElement(By.xpath("/html/body/div[1]/div/div/div/div/form/fieldset/fieldset[3]/input")).sendKeys(userPw);
     await browser.findElement(By.xpath("/html/body/div[1]/div/div/div/div/form/fieldset/button")).click();
+
+    //Wait for navigation to complete back to front page with new user logged in after Signing Up (default behaviour of app)
     const el = By.linkText(userName);
     browser.wait(until.elementLocated(el));
     const hrefEl = browser.findElement(el);
     browser.wait(until.elementIsVisible(hrefEl));
     const userText = await hrefEl.getText();
+    
+    //Placeholder console log to allow commit without TypeScript linter yelling
     // eslint-disable-next-line no-console
     console.log('User added successfully: ', userText);
-    //let source = await browser.getCurrentUrl();
     
+    // Now we move to posting a new article for the new user
+
+    // Note: During manual testing it has been discovered that clicking Publish Article Button has no visible effect
+    // and appears as if nothing was posted, user is left in edit.  By clicking on user's name top right, it becomes
+    // apparent that it did post but did not move to a different page or inform user.  Definetely a defect but beyond
+    // the scope of this assessment.
+    const postLinkText = 'New Article';
+    // const publishLinkText = 'Publish Article';
+
+    await browser.findElement(By.linkText(postLinkText)).click();
+
+    const et = By.xpath("/html/body/div/div/div/div/div/form/fieldset/button");
+    browser.wait(until.elementLocated(et));
+    const postEt = browser.findElement(et);
+    browser.wait(until.elementIsVisible(postEt));
+    // const publishText = await postEt.getText();
+    
+
+    await browser.findElement(By.xpath("/html/body/div/div/div/div/div/form/fieldset/fieldset[1]/input")).sendKeys(title);
+    await browser.findElement(By.xpath("/html/body/div/div/div/div/div/form/fieldset/fieldset[2]/input")).sendKeys(about);
+    await browser.findElement(By.xpath("/html/body/div/div/div/div/div/form/fieldset/fieldset[3]/textarea")).sendKeys(article);
+    await browser.findElement(By.xpath("/html/body/div/div/div/div/div/form/fieldset/button")).click();
+    //Workaround for no post on 'Publish Article' click
+    await browser.findElement(el).click();
+
+    //Same as before, this should have been turned into a function but for a small one off, works.  Mostly.
+    const ep = By.xpath('/html/body/div/div/div[2]/div/div/div[2]/a/h1');
+    browser.wait(until.elementLocated(ep));
+    const postEl = browser.findElement(ep);
+    browser.wait(until.elementIsVisible(postEl))
+    const postText = await postEl.getText();
+
+    // eslint-disable-next-line no-console
+    console.log('User post added successfully: ', postText);
     
     
 })();
